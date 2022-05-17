9ca6f8f6f77f494b624d10fd5ed1f60bfa77a155
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-05-17 12:30:05 +0200

    edit added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,9 +16,18 @@ class TasksController < ApplicationController
     @task.save
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
